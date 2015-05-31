0c145443c057528af28b78d89eb0842bc4a1e239
piotrekbass <piotrek.koscielniak@gmail.com>
2015-05-31 22:56:41 +0200

    homework 1

--- hello_express/app.js
+++ hello_express/app.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var cons = require('consolidate');
+var MongoClient = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
+
 var app = express();
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+var mongoclient = new MongoClient(new Server('192.168.56.102', 27017, {
+    native_parser: true
+}));
+var db = mongoclient.db('course');
+
 app.get('/', function (req, res) {
-    res.render('hello', {name: 'Swig'});
+    db.collection('hello_mongo_express').findOne({}, function (err, doc) {
+        if (err) throw  err;
+        res.render('hello', doc);
+    });
 });
 
 app.get('*', function (req, res) {
     res.send("Page not found!", 404);
 });
 
-app.listen(8080);
-console.log("Express server started on port 8080");
+mongoclient.open(function (err, mongoclient) {
+    if (err) throw  err;
+    app.listen(8080);
+    console.log("Express server started on port 8080");
+});
+
