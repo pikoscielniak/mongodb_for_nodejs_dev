5e0fb0a7a86350fc86e99120c26a64726dd9fa16
piotrekbass <piotrek.koscielniak@gmail.com>
2015-05-31 21:09:43 +0200

    added swig as a render engine

--- hello_express/app.js
+++ hello_express/app.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var cons = require('consolidate');
 var app = express();
 
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 app.get('/', function (req, res) {
-    res.send("Hello, World!");
+    res.render('hello', {name: 'Swig'});
 });
 
 app.get('*', function (req, res) {
